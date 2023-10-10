f18f885ed294950bc745913623f1176cf60790a0
ygowill <qq554552354@126.com>
2023-10-11 01:04:50 +0800

    update feature settings

--- rewrite.js
+++ rewrite.js
@@ -8,7 +8,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 if ("feature_gates" in obj) {
     for (var key in obj["feature_gates"]) {
-        if (("rule_id" in obj["feature_gates"][key]) && (obj["feature_gates"][key]["rule_id"] in ["3yqcjHol8kM4fbypFkzhxC:50.00:3"])) {
+        if (("rule_id" in obj["feature_gates"][key]) && (obj["feature_gates"][key]["rule_id"] === "3yqcjHol8kM4fbypFkzhxC:50.00:3")) {
             obj["feature_gates"][key]["value"] = true;
         }
     }
