cdc69a4e265d9f2e043c2b8308a48452bd1e3826
ygowill <qq554552354@126.com>
2023-10-11 00:59:00 +0800

    update feature settings

--- rewrite.js
+++ rewrite.js
@@ -1,18 +1,18 @@
 // JS script for quantumult x to rewrite chatgpt request, enable voice to voice feature
 
 var body = $response.body;
-console.log("===============");
-console.log($response);
-console.log("===============");
-if ($response.status === 200) {
-    var obj = JSON.parse(body);
-    if ("feature_gates" in obj) {
-        for (var key in obj["feature_gates"]) {
-            if (("rule_id" in obj["feature_gates"][key]) && (obj["feature_gates"][key]["rule_id"] in ["3yqcjHol8kM4fbypFkzhxC:50.00:3"])) {
-                obj["feature_gates"][key]["value"] = true;
-            }
+// console.log("===============");
+// console.log($response);
+// console.log("===============");
+// if ($response.status === 200) {
+var obj = JSON.parse(body);
+if ("feature_gates" in obj) {
+    for (var key in obj["feature_gates"]) {
+        if (("rule_id" in obj["feature_gates"][key]) && (obj["feature_gates"][key]["rule_id"] in ["3yqcjHol8kM4fbypFkzhxC:50.00:3"])) {
+            obj["feature_gates"][key]["value"] = true;
         }
     }
-    body = JSON.stringify(obj);
 }
+body = JSON.stringify(obj);
+// }
 $done(body);
