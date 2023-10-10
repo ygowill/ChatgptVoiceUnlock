7558240e9e24ae43b013b55db4e73f4f0eac7073
ygowill <qq554552354@126.com>
2023-10-11 00:53:39 +0800

    update feature settings

--- rewrite.js
+++ rewrite.js
@@ -1,6 +1,7 @@
 // JS script for quantumult x to rewrite chatgpt request, enable voice to voice feature
 
 var body = $response.body;
+console.log($response)
 if ($response.status === 200) {
     var obj = JSON.parse(body);
     if ("feature_gates" in obj) {
