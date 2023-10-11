54e62fc41a36cae6e05b71badd84802c6ec18dcc
ygowill <qq554552354@126.com>
2023-10-11 13:04:41 +0800

    try unlock vision

--- rewrite_beta_features.js
+++ rewrite_beta_features.js
@@ -1,16 +1,16 @@
 // JS script for quantumult x to rewrite chatgpt request, enable voice to voice feature
 
-var body = $response.body;
-if ($response.status === 200) {
-    var obj = {
-        "plugins": true,
-        "breeze": true,
-        "browsing": true,
-        "dalle": true,
-        "sunshine": true,
-        "code_interpreter": true
-    };
+// var body = $response.body;
 
-    body = JSON.stringify(obj);
-}
+var obj = {
+    "plugins": true,
+    "breeze": true,
+    "browsing": true,
+    "dalle": true,
+    "sunshine": true,
+    "code_interpreter": true
+};
+
+body = JSON.stringify(obj);
+
 $done(body);
